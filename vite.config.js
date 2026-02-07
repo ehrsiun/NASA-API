48d89ac07ab104bbaa52862ce1aedcc214430c51
erin <erinhsu@andrew.cmu.edu>
2026-02-07 17:07:20 -0500

    Fix gitignore and GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  // IMPORTANT: Replace YOUR_REPO_NAME_HERE with your GitHub repo name, including trailing slash.
-  // Example: repo "nasa-eonet-map" -> base: "/nasa-eonet-map/"
   base: "/NASA-API/"
 });
